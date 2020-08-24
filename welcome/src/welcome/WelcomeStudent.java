5d8c3a1de88b9303dd9fbe3a1143ff356cdcbc28
SHREE <SHREE@SHREE-PC>
2020-08-24 07:53:08 -0700

    Hiiii

--- welcome/src/welcome/WelcomeStudent.java
+++ welcome/src/welcome/WelcomeStudent.java
@@ -3,8 +3,7 @@ package welcome;
 public class WelcomeStudent {
 
 	public static void main(String[] args) {
-    System.out.println("welcome in java GitHub programming");
-    System.out.println("This is my First GitHub trial");			
+    System.out.println("welcome in java GitHub programming"); 		
 	}
 
 }
