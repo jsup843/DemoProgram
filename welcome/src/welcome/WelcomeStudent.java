3e4d7b72f021df4f9cf904e938f8f7a2b73f8523
jsup843 <70132752+jsup843@users.noreply.github.com>
2020-08-24 07:55:51 -0700

    Update WelcomeStudent.java

--- welcome/src/welcome/WelcomeStudent.java
+++ welcome/src/welcome/WelcomeStudent.java
@@ -3,6 +3,7 @@ package welcome;
 public class WelcomeStudent {
 
 	public static void main(String[] args) {
+		System.out.println("Hello, i'm Supeee.");
     System.out.println("welcome in java GitHub programming");
 		    System.out.println("welcome in java GitHub programming"); 		
 
